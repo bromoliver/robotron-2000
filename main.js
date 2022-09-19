b5f24caf91080acd6b7cf95baecdc7eaf4aecf82
bromoliver <96691382+bromoliver@users.noreply.github.com>
2022-09-19 08:49:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1
-})
+somar.addEventListener("click", () => {manipula_dados("somar")})
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) -1
-})
+subtrair.addEventListener("click", (evento) => {manipula_dados("subtrair")})
+
+function manipula_dados(operacao){
+    if(operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
+}
